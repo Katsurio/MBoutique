0b6f1070df1de5eca49b9b85ab2456564c780243
John (Jef) Fisher <Fishe545@gmail.com>
2017-01-21 01:52:03 -0800

    Saving to repository

---
+++ PHP Remake/pages/gifts_parties.php
@@ -0,0 +1,47 @@
+<?php
+$page_title = "MBoutique: Gifts and Parties Page";
+include("header.php");
+?>
+    <div class="bruce-banner">
+        <img src="../../assets/images/gifts-parties-image.png" alt="Macaron Gift Box">
+    </div>
+    <div class="main-content-wrapper">
+        <div class="om-top-row"> <!-- ("om" === "our-macarons"[reused for gifts & parties page]) -->
+            <h2>Let's eat macarons!</h2>
+            <br>
+            <p>We make it easy to share la passion du macaron with your friends, family and colleagues. Choose from our array of premium gift options, or we can create a custom solution.
+                Contact us when you need help planning your celebration! <a style="text-decoration: none; color: #df5277" href="mailto:order@mboutique.com?Subject=Hello%20again" class="mail" target="_top">orders@mboutique.com</a></p>
+            <br>
+        </div>
+        <div>
+
+        </div>
+        <div class="om-left-column" id="gp-position">
+            <h2>Gift boxes</h2>
+            <br>
+            <p>Our signature gift boxes make macarons the perfect gift for any occasion.</p>
+            <ul>
+                <li>gift box of 6 macarons in raspberry white - $ 10</li>
+                <li>gift box of 12 macarons in white, pistachio, or violet – $20</li>
+                <li>gift box of 24 macarons in violet – $40</li>
+                <li>gift box of 36 macarons in silver – $60</li>
+                <li>gift box à la ronde of 60 macarons in violet or silver – $120</li>
+            </ul>
+            <br>
+            <div id="party-tower">
+                <h2>Party tower</h2>
+                <br>
+                <p>Our 5-tiered party tower becomes the centerpiece of any special event or private celebration, elegantly presenting your unique selection of sixty macarons.</p>
+                <ul>
+                    <li>tower of 60 macarons – $120</li>
+                </ul>
+            </div>
+            <div id="om-left-column-background" class="stretch">
+                <img src="../../assets/images/gifts-parties-background.png" alt="Macaron Party Tower">
+            </div>
+        </div>
+        <p class="om-main-content-last-element"></p>
+    </div>
+<?php
+include("footer.php");
+?>
